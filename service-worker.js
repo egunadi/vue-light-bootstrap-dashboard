73e6a9f2f0fe19dd9f64af1be910052555d9ab64
Eben Gunadi <egunadi@usc.edu>
2023-11-19 12:08:27 -0800

    Rebuild dist

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b9007877eedf61d5057c9346952cd7fe.js"
+  "/vue-light-bootstrap-dashboard/precache-manifest.3e4a4c6191d900316cf21a4686391a11.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "vue-light-bootstrap-dashboard"});
