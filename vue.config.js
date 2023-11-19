54259f979312bb4268aeec917579bf90f459b24e
Eben Gunadi <egunadi@usc.edu>
2023-11-19 12:07:14 -0800

    Update vue.config.js for GH pages

--- vue.config.js
+++ vue.config.js
@@ -31,5 +31,6 @@ module.exports = {
   css: {
     // Enable CSS source maps.
     sourceMap: process.env.NODE_ENV !== 'production'
-  }
+  },
+  publicPath: process.env.NODE_ENV === "production" ? "/vue-light-bootstrap-dashboard/" : "/"
 };
